6f43f9ee47e3daeb6a6a1e61f710f51659340ad8
felix <Felix>
2022-01-31 05:06:36 +0300

    Edit level

--- Assets/Scripts/Levels/LevelHelper.cs
+++ Assets/Scripts/Levels/LevelHelper.cs
@@ -13,7 +13,7 @@ public class LevelHelper : MonoBehaviour
     private void Start()
     {
         Mobs = MobsSSS;
-        level = new Level00();
+        level = new Level99();
         StartCoroutine(level.StartLevel());
     }
 
